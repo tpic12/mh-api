6b87d3beea9903830fc0de42cf0512fe19fcffe7
tpic12 <tpic12@gmail.com>
2024-11-29 17:49:15 -0500

    logger

--- app/controllers/concerns/transformed_params.rb
+++ app/controllers/concerns/transformed_params.rb
@@ -2,7 +2,6 @@ module TransformedParams
   extend ActiveSupport::Concern
 
   def transformed_params
-    Rails.logger.info "Params before transformation: #{params.inspect}"
     params.permit!.to_h.symbolize_keys
   end
 end
